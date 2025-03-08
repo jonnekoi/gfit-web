5c4906a7ef0d9506bc2452068c2e3c751db207db
jonnekoi <jonnekoivisto00@gmail.com>
2025-03-09 00:36:14 +0200

    Created own components for selected meal and workout

--- src/components/mealComponents/AllMeals.jsx
+++ src/components/mealComponents/AllMeals.jsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faSort} from "@fortawesome/free-solid-svg-icons";
+import SelectedMealComponent from "./SelectedMealComponent.jsx";
 
 const URL = "http://127.0.0.1:3000/v1";
 
@@ -119,68 +120,7 @@ const AllMeals = () => {
                     </button>
                 </div>
                 {selectedMeal && (
-                    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-55">
-                        <div className="väribg text-white p-8 rounded max-w-5xl w-full text-center">
-                            <h2 className="font-bold montserrat-text text-2xl pb-2">{selectedMeal.meal_name || "Unnamed Meal"}</h2>
-                            <p className="montserrat-text font-bold text-1xl">{selectedMeal.meal_category}</p>
-                            <p className="montserrat-text m-2"></p>
-                            <p className="montserrat-text m-2">{selectedMeal.meal_description}</p>
-                            <div className="flex flex-row justify-center">
-                                <table>
-                                    <thead className="text-orange-500">
-                                    <tr>
-                                        <th className="m-2 montserrat-text p-5">Calories</th>
-                                        <th className="m-2 montserrat-text p-5">Protein</th>
-                                        <th className="m-2 montserrat-text p-5">Carbs</th>
-                                        <th className="m-2 montserrat-text p-5">Fat</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <tr>
-                                        <td>{Number (selectedMeal.total_calories).toFixed(1)} g</td>
-                                        <td>{Number(selectedMeal.total_protein).toFixed(1)} g</td>
-                                        <td>{Number (selectedMeal.total_carbs).toFixed(1)} g</td>
-                                        <td>{Number (selectedMeal.total_fat).toFixed(1)} g</td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <ul>
-                                <div className="flex justify-center">
-                                    <table className="w-full">
-                                        <thead className="">
-                                        <tr>
-                                            <th className="m-2 montserrat-text p-5">Name</th>
-                                            <th className="m-2 montserrat-text p-5">Weight (g)</th>
-                                            <th className="m-2 montserrat-text p-5">Protein / 100g</th>
-                                            <th className="m-2 montserrat-text p-5">Carbs / 100g</th>
-                                            <th className="m-2 montserrat-text p-5">Fat / 100g</th>
-                                            <th className="m-2 montserrat-text p-5">Calories / 100g</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        {ingredients.map((ingredient, index) => (
-                                            <tr key={index}>
-                                                <td className="m-2 poppins-text p-2 text-orange-500">{ingredient.ingredient_name}</td>
-                                                <td className="m-2 poppins-text p-2">{ingredient.ingredient_quantity_g}</td>
-                                                <td className="m-2 poppins-text p-2">{ingredient.protein_per_100g}</td>
-                                                <td className="m-2 poppins-text p-2">{ingredient.carbs_per_100g}</td>
-                                                <td className="m-2 poppins-text p-2">{ingredient.fat_per_100g}</td>
-                                                <td className="m-2 poppins-text p-2">{ingredient.calories_per_100g}</td>
-                                            </tr>
-                                        ))}
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </ul>
-                            <div className="flex flex-row justify-center">
-                                <button onClick={closeModal}
-                                        className="text-white bg-orange-500 font-bold p-2 mt-5 w-1/3 montserrat-text text-1xl border border-orange-500 rounded hover:border-orange-300">
-                                    Close
-                                </button>
-                            </div>
-                        </div>
-                    </div>
+                    <SelectedMealComponent selectedMeal={selectedMeal} ingredients={ingredients} closeModal={closeModal}/>
                 )}
             </div>
         </>
---
+++ src/components/workoutComponents/SelectedWorkoutModal.jsx
@@ -0,0 +1,125 @@
+import formatDate from "../../scripts/formatDate.js";
+
+const SelectedWorkoutModal = ({ selectedWorkout, editableExercises, isReadOnly, closeModal, handleInputChange, toggleEditMode, updateText }) => {
+
+    return (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-55">
+            <div className="väribg text-white p-8 rounded max-w-4xl w-full text-center">
+                <h2 className="font-bold montserrat-text text-2xl pb-2">
+                    {selectedWorkout.workout_name || "Unnamed Workout"}
+                </h2>
+                <p className="montserrat-text font-bold text-1xl">
+                    Crafted {formatDate(selectedWorkout.workout_created_at)}
+                </p>
+                <p className="montserrat-text m-2">{selectedWorkout.workout_type}</p>
+                <p className="montserrat-text m-2">
+                    {selectedWorkout.workout_description || "No description available"}
+                </p>
+
+                <table className="w-full text-white">
+                    <thead>
+                    <tr className="text-white">
+                        <th className="p-3 ">Exercise</th>
+                        <th className="p-3 ">Description</th>
+                        {isReadOnly ? (
+                            <>
+                                <th className="m-2 montserrat-text p-5">Reps</th>
+                                <th className="m-2 montserrat-text p-5">Sets</th>
+                                <th className="m-2 montserrat-text p-5">Weight</th>
+                            </>
+                        ) : (
+                            <>
+                                <th className="m-2 montserrat-text p-5">Reps Low</th>
+                                <th className="m-2 montserrat-text p-5">Reps Max</th>
+                                <th className="m-2 montserrat-text p-5">Sets</th>
+                                <th className="m-2 montserrat-text p-5">Weight</th>
+                            </>
+                        )}
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {editableExercises.map((exercise, index) => (
+                        <tr key={exercise.exercise_id} className="">
+                            <td className="m-2 poppins-text p-2 text-orange-500">
+                                {exercise.exercise_name}
+                            </td>
+                            <td className="m-2 poppins-text p-2">
+                                {exercise.exercise_description}
+                            </td>
+                            {isReadOnly ? (
+                                <>
+                                    <td className="m-2 poppins-text p-2">
+                                        {exercise.low_reps} - {exercise.max_reps}
+                                    </td>
+                                    <td className="m-2 poppins-text p-2">
+                                        {exercise.sets}
+                                    </td>
+                                    <td className="m-2 poppins-text p-2">
+                                        {exercise.weight}
+                                    </td>
+                                </>
+                            ) : (
+                                <>
+                                    <td className="p-2">
+                                        <input
+                                            type="number"
+                                            value={exercise.low_reps}
+                                            onChange={(e) =>
+                                                handleInputChange(index, "low_reps", e.target.value)
+                                            }
+                                            className="p-2 w-full rounded text-black"
+                                        />
+                                    </td>
+                                    <td className="p-2">
+                                        <input
+                                            type="number"
+                                            value={exercise.max_reps}
+                                            onChange={(e) =>
+                                                handleInputChange(index, "max_reps", e.target.value)
+                                            }
+                                            className="p-2 w-full rounded text-black"
+                                        />
+                                    </td>
+                                    <td className="p-2">
+                                        <input
+                                            type="number"
+                                            value={exercise.sets}
+                                            onChange={(e) =>
+                                                handleInputChange(index, "sets", e.target.value)
+                                            }
+                                            className="p-2 w-full rounded text-black"
+                                        />
+                                    </td>
+                                    <td className="p-2">
+                                        <input
+                                            type="number"
+                                            value={exercise.weight}
+                                            onChange={(e) =>
+                                                handleInputChange(index, "weight", e.target.value)
+                                            }
+                                            className="p-2 w-full rounded text-black"
+                                        />
+                                    </td>
+                                </>
+                            )}
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+                <div className="flex flex-row mt-5">
+                    <button onClick={toggleEditMode}
+                            className="text-white bg-orange-500 font-bold p-2 m-2 w-full montserrat-text text-1xl border border-orange-500 rounded hover:border-orange-300">
+                        {isReadOnly ? "Edit" : "Save"}
+                    </button>
+                    <button onClick={closeModal}
+                            className="text-white bg-orange-500 font-bold p-2 m-2 w-full montserrat-text text-1xl border border-orange-500 rounded hover:border-orange-300">
+                        Close
+                    </button>
+                </div>
+                {updateText && <p className="text-green-500 montserrat-text mt-5 text-1xl">{updateText}</p>}
+            </div>
+        </div>
+    )
+}
+
+export default SelectedWorkoutModal;
